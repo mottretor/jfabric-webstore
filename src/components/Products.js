9111c5176ef705205f77e22cb010a1bd8c69cbdc
PasinduPriyashan <pasindu95.m@gmail.com>
2018-05-21 12:57:19 +0530

    Finish Shopify Integration
    There are several tasks left to do.
    *Writting unit tests
    *Configure to use dotenv

---
+++ src/components/Products.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Product from './Product';
+
+class Products extends Component {
+    render() {
+        let products = this.props.products.map((product) => {
+            return (
+                <Product
+                    addVariantToCart={this.props.addVariantToCart}
+                    client={this.props.client}
+                    checkout={this.props.checkout}
+                    key={product.id.toString()}
+                    product={product}
+                />
+            );
+        });
+
+        return (
+            <div className="Product-wrapper">
+                {products}
+            </div>
+        );
+    }
+}
+
+export default Products;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Client from 'shopify-buy';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const client = Client.buildClient({
+    storefrontAccessToken: 'da44a78a7973ccdc095deab58bea7575',
+    domain: 'j-fabric-art-gallery.myshopify.com'
+});
+
+ReactDOM.render(<App client={client} />, document.getElementById('root'));
 registerServiceWorker();
